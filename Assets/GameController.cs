6841d182acf703cfab6995a5afadb2658bca9c40
hoatrinh <hoatv.7604@gmail.com>
2022-06-21 09:49:14 +0700

    + Add first demo.

---
+++ Assets/GameController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    [SerializeField] GameObject gameOver;
+    [SerializeField] Slider slider;
+    [SerializeField] TextMeshProUGUI prisoner;
+    [SerializeField] TextMeshProUGUI robber;
+    [SerializeField] TextMeshProUGUI highScoreText;
+
+    [SerializeField] SpawController spawController;
+    int score = 0;
+    int sliderValue = 0;
+
+    private float time;
+    private int highScore;
+    private void Start()
+    {
+        Reset();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 30)
+        {
+            spawController.UpdateSpeed();
+            time = 0;
+        }
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOver.SetActive(true);
+    }
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        prisoner.text = score + "";
+        robber.text = score + "";
+
+        if(highScore < score)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("score", highScore);
+            highScoreText.text = highScore + "";
+        }
+    }
+
+    public void UpdateSlider(int value)
+    {
+        sliderValue += value;
+        slider.value = sliderValue;
+
+        CheckGameState();
+    }
+
+    public void Restart()
+    {
+        GetComponent<SceneController>().StartGame();
+    }
+
+    public void CheckGameState()
+    {
+        if(sliderValue >= slider.maxValue)
+        {
+            GameOver();
+        }
+    }
+
+    public void Reset()
+    {
+        Time.timeScale = 1;
+        gameOver.SetActive(false);
+        sliderValue = 0;
+        score = 0;
+        highScore = PlayerPrefs.GetInt("score");
+        highScoreText.text = highScore + "";
+        slider.value = sliderValue;
+        prisoner.text = score + "";
+        robber.text = score + "";
+    }
+}
